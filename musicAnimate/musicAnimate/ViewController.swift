ff2ed1c894fd74f3ffaea345f9a5f021dbb59942
Kushal Krishna <kushal5070@gmail.com>
2023-02-17 12:58:24 +0530

    Add files via upload

---
+++ musicAnimate/musicAnimate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  musicAnimate
+//
+//  Created by APPLE on 17/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+    @IBOutlet weak var reverseBackground: UIView!
+   
+    @IBOutlet weak var playPauseBackground: UIView!
+    
+    @IBOutlet weak var forwardBackground: UIView!
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }}
+
